f951944388654a1311dd559334ebe98395c6ab4a
Mohitjain49 <mohitkjain49@gmail.com>
2025-03-30 16:32:58 -0400

    Replaced static copy plugin with cesium plugin.
    Cesium Plugin is Better.

--- vite.config.js
+++ vite.config.js
@@ -2,32 +2,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-const cesiumSource = "node_modules/cesium/Build/Cesium";
-const cesiumBaseUrl = "cesiumStatic";
+import vitePluginCesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-  base: "/",
-  server: { port: 5050 },
-  build: { chunkSizeWarningLimit: 5000 },
-  plugins: [
-    vue(),
-    viteStaticCopy({
-      targets: [
-        { src: `${cesiumSource}/ThirdParty`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Workers`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Assets`, dest: cesiumBaseUrl },
-        { src: `${cesiumSource}/Widgets`, dest: cesiumBaseUrl },
-      ],
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: "/",
+    server: { port: 5050 },
+    build: { chunkSizeWarningLimit: 5000 },
+    plugins: [
+        vue(),
+        vitePluginCesium({ rebuildCesium: false })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  },
-  define: {
-    CESIUM_BASE_URL: JSON.stringify(cesiumBaseUrl)
-  }
 });
