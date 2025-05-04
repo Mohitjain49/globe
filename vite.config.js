1498d4b98973bf21b899702fac56e51e5c84de7f
Mohitjain49 <mohitkjain49@gmail.com>
2025-05-04 13:14:54 -0400

    Setup deployment to GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 import vitePluginCesium from 'vite-plugin-cesium';
 
 export default defineConfig({
-    base: "/",
+    base: "/globe",
     server: { port: 5050 },
     build: { chunkSizeWarningLimit: 5000 },
     plugins: [
