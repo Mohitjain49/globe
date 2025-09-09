73d68469929c42fe6ca58521c413efbeebd645ec
Mohitjain49 <mohitkjain49@gmail.com>
2025-09-09 19:19:50 -0400

    Third time is the charm

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import NoMenu from "./menus/NoMenu.vue";
 import RedirectMenu from "./menus/RedirectMenu.vue";
 
@@ -29,7 +29,10 @@ export const globeViews = [
 ]
 
 const globeRouter = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: (TAURI_MODE ?
+        createWebHistory(import.meta.env.BASE_URL) :
+        createWebHashHistory(import.meta.env.BASE_URL)
+    ),
     routes: globeViews
 });
 
